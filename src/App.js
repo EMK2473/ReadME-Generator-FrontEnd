8ff435b51ab83b04021cda559f0b62179a157a66
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-14 03:28:30 -0500

    small edits for deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Form from "./Form"; // Update the path based on your file structure
-import "./App.css"; // You can keep the default styles or modify them as needed
+import Form from "./Form"; 
+import "./App.css"; 
 
 function App() {
   return (
